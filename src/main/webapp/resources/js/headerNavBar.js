28cf24d235aad6c01c7d9c7eb32de4ecbd88127b
Yoonseo Choi <98252597+yoonseo94@users.noreply.github.com>
2022-08-03 14:18:25 +0900

    fix(headerNavBar):img파일 contextpath경로지정
    기존 contextpath경로 js파일에서 기존코드로 못가져와서 수정

--- src/main/webapp/resources/js/headerNavBar.js
+++ src/main/webapp/resources/js/headerNavBar.js
@@ -1,6 +1,9 @@
+const ctx = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2))
+console.log(ctx);
 $(document).ready(function (){
+ 	
 	$('.nav_link').addClass('nav_color_b');
-	$('#title_img').attr('src','${pageContext.request.contextPath}/resources/images/title_blue.png')
+	$('#title_img').attr('src',ctx + '/resources/images/title_blue.png')
 	$(window).on('scroll', function(){
 		if(this.scrollY >= 0) {
 			$('.nav_link').addClass('nav_color_b');
